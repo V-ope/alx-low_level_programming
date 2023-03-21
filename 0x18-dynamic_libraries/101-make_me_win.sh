a4f4bb8eb13d7fa66f3246d36897da5aa63c1d5a
V-ope <opeyemivictor04@gmail.com>
2023-03-20 20:13:43 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/V-ope/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
+export LD_PRELOAD=$HOME/lib101-makemewin.so
